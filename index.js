e11005f2522fcc740d0cd66deaa8bf65ca0a7c78
Flor Palma <whatsgoingonlion@gmail.com>
2023-04-22 09:02:04 -0600

    tercer commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = http.createServer()
 server.on("request", (request, response) => {
   // handle request based on method then URL
   response.statusCode = 200
-  response.write("Hello World publicado")
+  response.write("Hello World publicado por Flor Palma")
   response.end()
 })
 // get the server to start listening
